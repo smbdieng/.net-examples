d1fbe50565794d5d04bc140c256caebbb5774801
sl <stefan.leopold@reflex.at>
2012-12-09 18:40:03 +0100

    initial

---
+++ WpfTest/Attach.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Media;
+
+namespace WpfTest
+{
+    public class Attach
+    {
+        public static DependencyProperty HoverBrushProperty =
+            DependencyProperty.RegisterAttached("HoverBrush",
+                                                typeof(Brush),
+                                                typeof(Attach),
+                                                new PropertyMetadata(null));
+        public static void SetHoverBrush(DependencyObject obj, Brush value)
+        {
+            obj.SetValue(HoverBrushProperty, value);
+        }
+        public static Brush GetHoverBrush(DependencyObject obj)
+        {
+            return (Brush)obj.GetValue(HoverBrushProperty);
+        }
+    }
+}
